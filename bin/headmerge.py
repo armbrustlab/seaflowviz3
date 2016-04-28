3177a2c09b4f4b7cf84e0f6fa3eee06395534322
Chris Berthiaume <chrisbee@uw.edu>
2016-04-28 16:38:26 -0700

    Move some functionality into seaflowpy. Remove some SQLShare code.

--- bin/headmerge.py
+++ bin/headmerge.py
@@ -1,20 +1,29 @@
 #!/usr/bin/env python
+"""Merge data files and only keep the header line for the first file."""
+import argparse
 import sys
 
-if len(sys.argv) > 1:
-    files = sys.argv[1:]
-else:
-    sys.exit(0)
 
-header_seen = False
-for filename in files:
-    with open(filename) as fh:
-        linenum = 0
-        for line in fh:
-            if linenum == 0:
-                if not header_seen:
+def main(files):
+    header_seen = False
+    for filename in sorted(files):
+        with open(filename) as fh:
+            linenum = 0
+            for line in fh:
+                if linenum == 0:
+                    if not header_seen:
+                        sys.stdout.write(line)
+                        header_seen = True
+                else:
                     sys.stdout.write(line)
-                    header_seen = True
-            else:
-                sys.stdout.write(line)
-            linenum += 1
+                linenum += 1
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(
+        description="""Merge data files. Only keep the header line for the
+        first file. File names are sorted before merging.""")
+    parser.add_argument("files", nargs="*",
+        help="Data files with identical one line headers.")
+    args = parser.parse_args()
+    main(args.files)
